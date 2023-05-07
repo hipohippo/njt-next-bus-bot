6821c3da31aa10fbd649ea86e91d56c03d396423
hipo <hipo@vultr.guest>
2023-05-07 02:07:19 -0400

    minor change

--- src/main.py
+++ src/main.py
@@ -7,7 +7,6 @@ from hipo_telegram_bot_common.bot_config.bot_config_parser import parse_from_ini
 from hipo_telegram_bot_common.bot_factory import BotBuilder
 from hipo_telegram_bot_common.common_handler import heart_beat_job
 from njt_next_bus_bot.bot_handler import next_bus_handler, next_bus_pabt_handler, next_bus_lhny_handler
-from njt_next_bus_bot.local_test import get_config_from_local_dev
 from njt_next_bus_bot.njt_next_bus_bot_config import NJTNextBusBotConfig
 
 
@@ -28,16 +27,9 @@ def build_bot_app(bot_config_dict) -> Application:
     return bot_app
 
 
-def get_bot_config_dict(mode) -> dict:
-    if mode == "PROD":
-        bot_config_file = sys.argv[2]
-        bot_config_dict = parse_from_ini(bot_config_file)
-    else:
-        bot_config_dict = get_config_from_local_dev()
-    return bot_config_dict
-
-
 if __name__ == "__main__":
     logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
-    bot_app = build_bot_app(get_bot_config_dict("DEV"))
+    bot_config_file = sys.argv[2]
+    bot_config_dict = parse_from_ini(bot_config_file)
+    bot_app = build_bot_app(bot_config_dict)
     bot_app.run_polling()
